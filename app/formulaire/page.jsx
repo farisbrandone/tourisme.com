24c9f8fb6dfa2770222954e53181bed1b4eecfa8
farisbrandone <farisbrandone@yahoo.com>
2024-05-19 01:41:04 +0100

    correction on card number

--- app/formulaire/page.jsx
+++ app/formulaire/page.jsx
@@ -187,7 +187,7 @@ export default function Page() {
           selectedCardName={selectedCardName}
           setSelectedCardName={setSelectedCardName}
           selectedCardNumber={selectedCardNumber}
-          setSelectedCardCardNumber={setSelectedCardNumber}
+          setSelectedCardNumber={setSelectedCardNumber}
           selectedMonthExp={selectedMonthExp}
           setSelectedMonthExp={setSelectedMonthExp}
           selectedYearsExp={selectedYearsExp}
